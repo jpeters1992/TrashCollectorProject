8b69215d3c8e83d3d695f713299680132d1031c7
DESKTOP-CJ5TJR4\MASENAK <peter736@uwm.edu>
2019-10-29 14:18:55 -0500

    I added more code to my Employee Controller and built out the CRUD for the Employee Views

--- TrashCollectorProject/TrashCollector/TrashCollector/Controllers/EmployeesController.cs
+++ TrashCollectorProject/TrashCollector/TrashCollector/Controllers/EmployeesController.cs
@@ -3,15 +3,106 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
     public class EmployeesController : Controller
     {
+        ApplicationDbContext context;
+
+        //CONSTRUCTOR 
+        public EmployeesController()
+        {
+            context = new ApplicationDbContext();
+        }
+
         // GET: Employees
         public ActionResult Index()
         {
+            //code needed
             return View();
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int id)
+        {
+            Employee employee = context.Employees.Where(e => e.Id == id).SingleOrDefault();
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            Employee employee = new Employee();
+            return View(employee);
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            try
+            {
+                //code needed
+                context.Customers.Add(employee);
+                context.SaveChanges();
+                return RedirectToAction("code needed");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Employee employee = context.Employees.Where(e => e.Id == id).SingleOrDefault();
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Employee employee)
+        {
+            try
+            {
+                Employee editedEmployee = context.Employees.Find(id);
+                editedEmployee.FirstName = employee.FirstName;
+                editedEmployee.LastName = employee.LastName;
+                editedEmployee.ZipCode = employee.ZipCode;
+                context.SaveChanges();
+
+                return RedirectToAction("code needed");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Employee employee = context.Employees.Where(e => e.Id == id).SingleOrDefault();
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Employee employee)
+        {
+            try
+            {
+                context.Employees.Remove(context.Employees.Find(id));
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
